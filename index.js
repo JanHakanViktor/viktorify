88789a1d7b549751207ae38aca365d5121e619a1
JanHakanViktor <Janhakanv@gmail.com>
2025-09-26 19:59:11 +0200

    looping through each song in music array, added button to mark favorite and added css props.

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-(async () => {
+async function renderAllSongs() {
   const response = await fetch("musicLibrary.json");
   const data = await response.json();
-  const songs = data.music;
+  const allSongs = data.music;
 
-  document.getElementById("renderSongs").addEventListener("click", () => {
-    console.log(songs);
+  const songContainer = document.getElementById("songList");
+
+  allSongs.forEach((song) => {
+    const songDiv = document.createElement("div");
+    songDiv.className = "songDiv";
+    songDiv.textContent = `${song.title} by ${song.artist}`;
+
+    const buttonFavorite = document.createElement("button");
+    buttonFavorite.textContent = "Mark as favorite";
+
+    const albumCover = document.createElement("img");
+    albumCover.src = song.image;
+    albumCover.className = "albumCover";
+
+    songContainer.append(songDiv);
+    songDiv.append(albumCover, buttonFavorite);
   });
-})();
+}
+
+renderAllSongs();
